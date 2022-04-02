2ee7e282ded8abc6319e7d851593e85ec7bf2f4a
missamaral <flaviamaral@gmail.com>
2022-04-02 11:33:10 -0300

    Practice exercises

---
+++ 04 - JavaScript/04 - Modules/funcoes.mjs
@@ -0,0 +1,15 @@
+function showAge(name, age) {
+    return `${name} is ${age} years old.`
+}
+
+function showCity(name, city) {
+    return `${name} lives in ${city}.`
+}
+
+function showHobby(name, hobby) {
+    return `${name}'s favorite hobby is ${hobby}.`
+}
+
+export {
+    showAge, showCity, showHobby
+};
---
+++ 04 - JavaScript/04 - Modules/main.mjs
@@ -0,0 +1,5 @@
+import { showAge, showCity, showHobby } from "./funcoes.mjs";
+
+console.log(showAge("Flávia", "38"));
+console.log(showCity("Flávia", "São Bernardo do Campo"));
+console.log(showHobby("Flávia","learning languages"));
